2177e38551b723e3bc58ea6d3521956a5e863e9c
mmmeqqq <bew.bussayakorn@gmail.com>
2024-06-10 10:02:01 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './component/Home'
+import Owner from './component/Owner'
+import Nav from './component/Nav'
+
+
+const App = () => {
+  return (
+    <div>
+        <Nav></Nav>
+        <Home></Home>
+        <Owner></Owner>       
+    </div>
+  )
+}
+
+export default App
